9185e25c3518cf54518a49dd91365eb11133bdf1
areibman <meta.alex.r@gmail.com>
2020-01-22 23:04:40 -0500

    Maybe this will work?

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,5 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(
-    proxy(["/api", "/otherApi"], { target: `${process.env.PUBLIC_URL}` })
-  );
+  app.use(proxy(["/api", "/otherApi"], { target: "docit-web.herokuapp.com" }));
 };
