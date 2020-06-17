ec3d1549383ec964536151c309da0ff7a40f072b
areibman <meta.alex.r@gmail.com>
2020-06-16 21:40:21 -0400

    load dotenv

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@
 // const port = process.env.PORT || 5000;
 
 // // Load dotenv
-// const dotenv = require("dotenv");
-// dotenv.config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
