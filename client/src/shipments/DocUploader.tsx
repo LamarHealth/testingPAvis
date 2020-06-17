bb8c534b782796c1d54d1b8b0b7330f1b35d4b3a
Carter Chen <carterchen@Carters-MBP.attlocal.net>
2020-06-17 13:46:23 -0400

    added sidebar styling

--- client/src/shipments/DocUploader.tsx
+++ client/src/shipments/DocUploader.tsx
@@ -6,107 +6,112 @@ import { colors } from "../common/colors";
 import { UploadingList } from "./UploadThumbnails";
 
 const getColor = (props: any) => {
-  if (props.isDragAccept) {
-    return "#00e676";
-  }
-  if (props.isDragReject) {
-    return "#ff1744";
-  }
-  if (props.isDragActive) {
-    return "#2196f3";
-  }
-  return "lightgray";
+	if (props.isDragAccept) {
+		return "#00e676";
+	}
+	if (props.isDragReject) {
+		return "#ff1744";
+	}
+	if (props.isDragActive) {
+		return "#2196f3";
+	}
+	return "lightgray";
 };
 
 const Container = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border-width: 2px;
-  border-radius: 2px;
-  border-color: ${props => getColor(props)};
-  border-style: dashed;
-  background-color: white;
-  color: gray;
-  outline: none;
-  transition: border 0.24s ease-in-out;
-  transition: background-color 0.24s ease-in-out;
-  text-align: center;
-  justify-content: center;
-  padding: 1em 0;
-  :hover {
-    background-color: ${colors.LIGHTBLUE};
-    color: white;
-    border-color: white;
-  }
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	border-width: 2px;
+	border-radius: 2px;
+	border-color: ${(props) => getColor(props)};
+	border-style: dashed;
+	background-color: white;
+	color: gray;
+	outline: none;
+	transition: border 0.24s ease-in-out;
+	transition: background-color 0.24s ease-in-out;
+	text-align: center;
+	justify-content: center;
+	padding: 1em 0;
+	:hover {
+		background-color: hsla(211, 96%, 9%, 0.15);
+		color: rgb(1, 23, 47);
+		border-color: rgb(1, 23, 47);
+	}
+	margin: 2em;
+
+	& > p {
+		padding: 1em;
+	}
 `;
 
 export interface IFileWithPreview {
-  file: File;
-  preview: string;
+	file: File;
+	preview: string;
 }
 
 export const StyledDropzone = () => {
-  // Files can be declared as an object that we can later iterate over with object.entries()
-  const [newFiles, setNewFiles] = useState([] as IFileWithPreview[]);
+	// Files can be declared as an object that we can later iterate over with object.entries()
+	const [newFiles, setNewFiles] = useState([] as IFileWithPreview[]);
 
-  // On Drop
-  const onDrop = React.useCallback(
-    (acceptedFiles: Array<File>) => {
-      // Create dictionary containing file and preview
-      const filesWithThumbnails = acceptedFiles.map((file: File) => {
-        return {
-          file,
-          preview: URL.createObjectURL(file)
-        };
-      });
+	// On Drop
+	const onDrop = React.useCallback(
+		(acceptedFiles: Array<File>) => {
+			// Create dictionary containing file and preview
+			const filesWithThumbnails = acceptedFiles.map((file: File) => {
+				return {
+					file,
+					preview: URL.createObjectURL(file),
+				};
+			});
 
-      setNewFiles(newFiles.concat(filesWithThumbnails));
+			setNewFiles(newFiles.concat(filesWithThumbnails));
 
-      // endblock
-    },
-    [newFiles]
-  );
+			// endblock
+		},
+		[newFiles]
+	);
 
-  // onDragOver- Use this to make fancy animations with other components
-  const onDragOver = React.useCallback((acceptedFiles: any) => {
-    console.log("dragging");
-  }, []);
+	// onDragOver- Use this to make fancy animations with other components
+	const onDragOver = React.useCallback((acceptedFiles: any) => {
+		console.log("dragging");
+	}, []);
 
-  useEffect(
-    () => () => {
-      // Make sure to revoke the data uris to avoid memory leaks
-      newFiles.forEach((fileWithPreview: IFileWithPreview) =>
-        URL.revokeObjectURL(fileWithPreview.preview)
-      );
-    },
-    [newFiles]
-  );
+	useEffect(
+		() => () => {
+			// Make sure to revoke the data uris to avoid memory leaks
+			newFiles.forEach((fileWithPreview: IFileWithPreview) =>
+				URL.revokeObjectURL(fileWithPreview.preview)
+			);
+		},
+		[newFiles]
+	);
 
-  // Dropzone Hook
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject
-  } = useDropzone({
-    accept: ["application/pdf", "image/*"],
-    onDrop,
-    onDragOver
-  });
+	// Dropzone Hook
+	const {
+		getRootProps,
+		getInputProps,
+		isDragActive,
+		isDragAccept,
+		isDragReject,
+	} = useDropzone({
+		accept: ["application/pdf", "image/*"],
+		onDrop,
+		onDragOver,
+	});
 
-  return (
-    <>
-      <Container
-        {...getRootProps({ isDragActive, isDragAccept, isDragReject })}
-      >
-        <input {...getInputProps()} />
-        <p>Drag and drop or click to select files</p>
-        <Icon icon={"cloud-upload"} iconSize={25} />
-      </Container>
-      {!newFiles.length || <UploadingList files={newFiles} />}
-    </>
-  );
+	return (
+		<>
+			<Container
+				{...getRootProps({ isDragActive, isDragAccept, isDragReject })}
+			>
+				<input {...getInputProps()} />
+				<p>Drag and drop or click to select files</p>
+				<Icon icon={"cloud-upload"} iconSize={25} />
+			</Container>
+			{!newFiles.length || <UploadingList files={newFiles} />}
+		</>
+	);
 };
--- client/src/shipments/DocViewer.tsx
+++ client/src/shipments/DocViewer.tsx
@@ -1,9 +1,9 @@
 import React, {
-  useReducer,
-  useState,
-  createContext,
-  useContext,
-  useEffect,
+	useReducer,
+	useState,
+	createContext,
+	useContext,
+	useEffect,
 } from "react";
 import styled from "styled-components";
 import { StyledDropzone } from "./DocUploader";
@@ -11,21 +11,21 @@ import { Icon, Button, Popover, Menu } from "@blueprintjs/core";
 import $ from "jquery";
 import { act } from "@testing-library/react";
 interface IDocumentList {
-  documents: Array<DocumentInfo>;
+	documents: Array<DocumentInfo>;
 }
 
 export interface DocumentInfo {
-  docType: String;
-  docName: String;
-  docClass: String;
-  filePath: String;
-  docID: String;
-  keyValuePairs: Object;
+	docType: String;
+	docName: String;
+	docClass: String;
+	filePath: String;
+	docID: String;
+	keyValuePairs: Object;
 }
 
 export interface IFileDispatch {
-  type: String;
-  documentInfo: DocumentInfo;
+	type: String;
+	documentInfo: DocumentInfo;
 }
 
 export const CountContext = createContext({} as any);
@@ -35,225 +35,239 @@ export const FileContext = createContext({} as any);
  * Sidebar column container
  */
 const Column = styled.div`
-  justify-content: flex-start;
-  flex-direction: column;
-  align-items: stretch;
-  margin: 1em 0em;
-  background-color: lightgray;
-  border: solid;
-  display: inline-block;
-  height: 100%;
-  width: 25%;
-  margin-left: ${(props: { open: boolean }) =>
-    props.open ? "calc(-25% )" : "0.5em"};
-  overflow: auto;
-  padding: 0px 5px 1em;
-  transition: all 1s;
+	justify-content: flex-start;
+	flex-direction: column;
+	align-items: stretch;
+	margin: 1em 0em;
+	border: 2px solid rgb(1, 23, 47);
+	background-color: hsla(32, 100%, 97%, 0.5);
+	border-radius: 10px;
+	display: inline-block;
+	height: 100%;
+	width: 25%;
+	margin-left: ${(props: { open: boolean }) =>
+		props.open ? "calc(-25% )" : "0.5em"};
+	overflow: auto;
+	transition: all 1s;
 `;
 
 const ExpandButton = styled.button`
-  position: relative;
-  width: 2em;
-  height: 3em;
-  top: 50%;
-  right: 1.5em;
-  margin: 1em 1em 1em 1em;
-  padding: 0;
-  display: flex;
-  justify-content: center;
+	position: relative;
+	width: 2em;
+	height: 3em;
+	top: 50%;
+	right: 1em; //CCC: changed from 1.5 to 1 to keep button from overlapping the div
+	margin: 1em 1em 1em 1em;
+	padding: 0;
+	display: flex;
+	justify-content: center;
+	background-color: rgb(1, 23, 47);
+	color: rgb(255, 248, 240);
+	opacity: 0.4;
+	transition: 0.5s;
+	border: none;
+	border-radius: 0% 25% 25% 0%;
+
+	&:hover {
+		opacity: 1;
+	}
 `;
 
 const Chevron = styled(Icon)`
-  position: relative;
+	position: relative;
+	top: 25%; //CCC: to get chevron in middle... idk exactly why this works but it does
 `;
 
 /**
  * Cell containing doc info
  */
+
+const Instructions = styled.div`
+	text-align: center;
+	padding: 2em 2em 0em 2em;
+	color: rgb(1, 23, 47);
+`;
+
 const Box = styled.div`
-  margin: 1em 0em;
-  padding: 5px;
-  border: solid;
-  background-color: white;
-  overflow: auto;
+	margin: 1em 0em;
+	padding: 5px;
+	border: solid;
+	background-color: white;
+	overflow: auto;
 `;
 const Name = styled.h2``;
 const Type = styled.h4`
-  display: flex;
+	display: flex;
 `;
 
 const RemoveButton = styled(Button)`
-  top: 90%;
-  left: 90%;
+	top: 90%;
+	left: 90%;
 `;
 
 const DeleteDialog = (props: { setIsOpen: any; document: DocumentInfo }) => {
-  const fileInfoContext = useContext(FileContext);
+	const fileInfoContext = useContext(FileContext);
 
-  return (
-    <Menu>
-      <a
-        className="bp3-menu-item"
-        onClick={() => {
-          fileInfoContext.fileDispatch({
-            type: "remove",
-            documentInfo: props.document,
-          });
-          props.setIsOpen(false);
-        }}
-      >
-        <Icon icon={"trash"} />
-        Confirm Delete
-      </a>
-    </Menu>
-  );
+	return (
+		<Menu>
+			<a
+				className="bp3-menu-item"
+				onClick={() => {
+					fileInfoContext.fileDispatch({
+						type: "remove",
+						documentInfo: props.document,
+					});
+					props.setIsOpen(false);
+				}}
+			>
+				<Icon icon={"trash"} />
+				Confirm Delete
+			</a>
+		</Menu>
+	);
 };
 const DeleteDialogContext = createContext({} as any);
 const useDeleteDialogContext = () => {
-  const context = useContext(DeleteDialogContext);
-  return context;
+	const context = useContext(DeleteDialogContext);
+	return context;
 };
 
 const populateForms = () => {
-  $(document).ready(function () {
-    $("div")
-      .find("input[id=text-input1]")
-      .each(function (ev) {
-        if (!$(this).val()) {
-          $(this).attr("value", "Date: 7/5/2019");
-        }
-      });
-  });
+	$(document).ready(function () {
+		$("div")
+			.find("input[id=text-input1]")
+			.each(function (ev) {
+				if (!$(this).val()) {
+					$(this).attr("value", "Date: 7/5/2019");
+				}
+			});
+	});
 
-  $(document).ready(function () {
-    $("div")
-      .find("input[id=text-input2]")
-      .each(function (ev) {
-        if (!$(this).val()) {
-          $(this).attr("value", "City/State/Zip San Jose, California 57293");
-        }
-      });
-  });
+	$(document).ready(function () {
+		$("div")
+			.find("input[id=text-input2]")
+			.each(function (ev) {
+				if (!$(this).val()) {
+					$(this).attr("value", "City/State/Zip San Jose, California 57293");
+				}
+			});
+	});
 
-  $(document).ready(function () {
-    $("div")
-      .find("input[id=text-input3]")
-      .each(function (ev) {
-        if (!$(this).val()) {
-          $(this).attr("value", "477195");
-        }
-      });
-  });
+	$(document).ready(function () {
+		$("div")
+			.find("input[id=text-input3]")
+			.each(function (ev) {
+				if (!$(this).val()) {
+					$(this).attr("value", "477195");
+				}
+			});
+	});
 
-  $(document).ready(function () {
-    $("div")
-      .find("input[id=text-input4]")
-      .each(function (ev) {
-        if (!$(this).val()) {
-          $(this).attr("value", "CID#: 13144-f-6885");
-        }
-      });
-  });
+	$(document).ready(function () {
+		$("div")
+			.find("input[id=text-input4]")
+			.each(function (ev) {
+				if (!$(this).val()) {
+					$(this).attr("value", "CID#: 13144-f-6885");
+				}
+			});
+	});
 
-  $(document).ready(function () {
-    $("div")
-      .find("input[id=text-input5]")
-      .each(function (ev) {
-        if (!$(this).val()) {
-          $(this).attr("value", "SID# 321312-a-2131");
-        }
-      });
-  });
+	$(document).ready(function () {
+		$("div")
+			.find("input[id=text-input5]")
+			.each(function (ev) {
+				if (!$(this).val()) {
+					$(this).attr("value", "SID# 321312-a-2131");
+				}
+			});
+	});
 
-  $(document).ready(function () {
-    $("div")
-      .find("input[id=text-input6]")
-      .each(function (ev) {
-        if (!$(this).val()) {
-          $(this).attr("value", "FOB x");
-        }
-      });
-  });
+	$(document).ready(function () {
+		$("div")
+			.find("input[id=text-input6]")
+			.each(function (ev) {
+				if (!$(this).val()) {
+					$(this).attr("value", "FOB x");
+				}
+			});
+	});
 
-  $(document).ready(function () {
-    $("div")
-      .find("input[id=text-input7]")
-      .each(function (ev) {
-        if (!$(this).val()) {
-          $(this).attr("value", "COD Amount: $");
-        }
-      });
-  });
+	$(document).ready(function () {
+		$("div")
+			.find("input[id=text-input7]")
+			.each(function (ev) {
+				if (!$(this).val()) {
+					$(this).attr("value", "COD Amount: $");
+				}
+			});
+	});
 
-  $(document).ready(function () {
-    $("div")
-      .find("input[id=text-input8]")
-      .each(function (ev) {
-        if (!$(this).val()) {
-          $(this).attr("value", "BAR CODE SPACE");
-        }
-      });
-  });
+	$(document).ready(function () {
+		$("div")
+			.find("input[id=text-input8]")
+			.each(function (ev) {
+				if (!$(this).val()) {
+					$(this).attr("value", "BAR CODE SPACE");
+				}
+			});
+	});
 };
 
 const DocCell = (props: DocumentInfo) => {
-  const [modalIsOpen, setIsOpen] = useState(false);
-  return (
-    <Box>
-      <Name>{props.docName}</Name>
-      <Type>
-        <Icon icon={"rotate-document"} />
-        Document Type: {props.docClass}
-      </Type>
-      <Type>Format: {props.docType}</Type>
-      <Button onClick={populateForms}>Complete Forms on Page</Button>
-      <Popover
-        isOpen={modalIsOpen}
-        content={<DeleteDialog setIsOpen={setIsOpen} document={props} />}
-      >
-        <RemoveButton
-          onClick={() => {
-            setIsOpen(true);
-          }}
-        >
-          <Icon icon={"delete"} />
-        </RemoveButton>
-      </Popover>
-    </Box>
-  );
+	const [modalIsOpen, setIsOpen] = useState(false);
+	return (
+		<Box>
+			<Name>{props.docName}</Name>
+			<Type>
+				<Icon icon={"rotate-document"} />
+				Document Type: {props.docClass}
+			</Type>
+			<Type>Format: {props.docType}</Type>
+			<Button onClick={populateForms}>Complete Forms on Page</Button>
+			<Popover
+				isOpen={modalIsOpen}
+				content={<DeleteDialog setIsOpen={setIsOpen} document={props} />}
+			>
+				<RemoveButton
+					onClick={() => {
+						setIsOpen(true);
+					}}
+				>
+					<Icon icon={"delete"} />
+				</RemoveButton>
+			</Popover>
+		</Box>
+	);
 };
 
 const InstructionsCell = () => {
-  return (
-    <Box>
-      <Type>Select your files to get started </Type>
-    </Box>
-  );
+	return <Instructions>Add files to get started</Instructions>;
 };
 const removeDocument = (docID: String) => {
-  const newDocList = JSON.parse(localStorage.getItem("docList") || "[]").filter(
-    (item: DocumentInfo) => item.docID !== docID
-  );
-  localStorage.setItem("docList", JSON.stringify(newDocList));
-  return {
-    documents: JSON.parse(localStorage.getItem("docList") || "[]"),
-  };
+	const newDocList = JSON.parse(localStorage.getItem("docList") || "[]").filter(
+		(item: DocumentInfo) => item.docID !== docID
+	);
+	localStorage.setItem("docList", JSON.stringify(newDocList));
+	return {
+		documents: JSON.parse(localStorage.getItem("docList") || "[]"),
+	};
 };
 
 export const fileReducer = (
-  state: IDocumentList,
-  action: IFileDispatch
+	state: IDocumentList,
+	action: IFileDispatch
 ): IDocumentList => {
-  switch (action.type) {
-    case "append":
-      return {
-        documents: [...JSON.parse(localStorage.getItem("docList") || "[]")],
-      };
-    case "remove":
-      return removeDocument(action.documentInfo.docID);
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case "append":
+			return {
+				documents: [...JSON.parse(localStorage.getItem("docList") || "[]")],
+			};
+		case "remove":
+			return removeDocument(action.documentInfo.docID);
+		default:
+			return state;
+	}
 };
 
 /**
@@ -263,38 +277,45 @@ export const fileReducer = (
  */
 
 const initialState = {
-  documents: JSON.parse(localStorage.getItem("docList") || "[]"),
+	documents: JSON.parse(localStorage.getItem("docList") || "[]"),
 } as IDocumentList;
 const DocViewer = () => {
-  const [fileList, fileDispatch] = useReducer(fileReducer, initialState);
-  const [isOpen, setOpen] = useState(true);
+	const [fileList, fileDispatch] = useReducer(fileReducer, initialState);
+	const [isOpen, setOpen] = useState(true);
 
-  return (
-    <FileContext.Provider value={{ fileList, fileDispatch }}>
-      <Column open={isOpen}>
-        {fileList.documents.length ? (
-          fileList.documents.map((doc: DocumentInfo, ndx: any) => (
-            <DocCell
-              docName={doc.docName}
-              docType={doc.docType}
-              filePath={doc.filePath}
-              docClass={doc.docClass}
-              docID={doc.docID}
-              keyValuePairs={doc.keyValuePairs}
-              key={ndx}
-            />
-          ))
-        ) : (
-          <InstructionsCell />
-        )}
+	// CCC didn't know where else to put this guy
+	const buttonOpacity = () => {
+		if (!isOpen) {
+			return { opacity: 1 };
+		}
+	};
 
-        <StyledDropzone />
-      </Column>
-      <ExpandButton onClick={() => setOpen(!isOpen)}>
-        <Chevron icon={isOpen ? "chevron-right" : "chevron-left"} />
-      </ExpandButton>
-    </FileContext.Provider>
-  );
+	return (
+		<FileContext.Provider value={{ fileList, fileDispatch }}>
+			<Column open={isOpen}>
+				{fileList.documents.length ? (
+					fileList.documents.map((doc: DocumentInfo, ndx: any) => (
+						<DocCell
+							docName={doc.docName}
+							docType={doc.docType}
+							filePath={doc.filePath}
+							docClass={doc.docClass}
+							docID={doc.docID}
+							keyValuePairs={doc.keyValuePairs}
+							key={ndx}
+						/>
+					))
+				) : (
+					<InstructionsCell />
+				)}
+
+				<StyledDropzone />
+			</Column>
+			<ExpandButton onClick={() => setOpen(!isOpen)} style={buttonOpacity()}>
+				<Chevron icon={isOpen ? "chevron-right" : "chevron-left"} />
+			</ExpandButton>
+		</FileContext.Provider>
+	);
 };
 
 export default DocViewer;
