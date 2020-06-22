758ee66530139ccee02be0d299e0a391ed62dd81
areibman <meta.alex.r@gmail.com>
2020-06-21 20:54:56 -0400

    hacky fix?

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Frame from "react-frame-component";
+import { StyleSheetManager } from "styled-components";
+import Frame, { FrameContextConsumer } from "react-frame-component";
 import $ from "jquery";
 import "./index.css";
 import DocViewer from "./shipments/DocViewer";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import App from "./App";
 // blueprintjs
 import "@blueprintjs/core/lib/css/blueprint.css";
 
@@ -17,10 +17,19 @@ dotenv.config();
 //   '<div id="insertion-point" style="width:100%;height:100%;z-index:99999;position:fixed;" />'
 //   //   '<iframe id="insertion-point" allowtransparency="true" style=" overflow: hidden; position: fixed; right: 0px; top: 0px; left: auto; float: none; z-index: 2147483647; background: transparent;"/>'
 // ).insertBefore(document.body);
+const initialContent = `<!DOCTYPE html><html><head>${document.head.innerHTML}</head><body><div></div></body></html>`;
 
 ReactDOM.render(
-  <Frame>
-    <DocViewer />
+  <Frame initialContent={initialContent}>
+    <FrameContextConsumer>
+      {(frameContext) => (
+        <StyleSheetManager target={frameContext.document.head}>
+          <React.Fragment>
+            <DocViewer />
+          </React.Fragment>
+        </StyleSheetManager>
+      )}
+    </FrameContextConsumer>
   </Frame>,
   document.body
 );
