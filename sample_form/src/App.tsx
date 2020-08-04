1a476d7a8f2454b82e6d6e43624ed75c768fef3e
areibman <meta.alex.r@gmail.com>
2020-08-04 17:38:05 -0400

    New react app for demo testing

---
+++ sample_form/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FormDashboard from "./FormDashboard";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FormDashboard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
