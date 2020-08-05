be1c29d6b178cc45dc4f99694c7fb72aae713db2
areibman <meta.alex.r@gmail.com>
2020-08-05 02:17:46 -0400

    dashboard change?

--- client/src/shipments/ShipmentsDashboard.tsx
+++ client/src/shipments/ShipmentsDashboard.tsx
@@ -30,79 +30,41 @@ class ShipmentsDashboard extends React.Component {
       margin: 2em;
     `;
     return (
-      <>
-        {/* default form */}
-        {/* <Container>
-          <ChecklistColumn>
-            <h1>Sample Shipping Form</h1>
-            <FormGroup
-              helperText="Helper text with details..."
-              label="Shipping Instructions Required Fields"
-              labelFor="text-input"
-              labelInfo="(required)"
-            >
-              <h3>Date</h3>
-              <FormContainer key={1} id="text-input1" placeholder="Day" />
-              <FormContainer placeholder="Month" />
-              <FormContainer placeholder="Year" />
-              <FormContainer placeholder="Date" />
-              <h3>Shipment</h3>
-              <FormContainer placeholder="Description" />
-              <FormContainer placeholder="Consigned To" />
-              <FormContainer placeholder="Bill of Lading Number" />
-              <FormContainer placeholder="Carrier Booking" />
-              <FormContainer placeholder="Consignor" />
-              <FormContainer placeholder="Email" />
-              <FormContainer placeholder="Telephone" />
-              <FormContainer placeholder="SCAC" />
-              <FormContainer placeholder="CID" />
-            </FormGroup>
-            <FormGroup label="Additional info">
-              <FormContainer placeholder="SID" />
+      <Container>
+        <ChecklistColumn>
+          <h1>Sample Shipping Form</h1>
+          <FormGroup
+            helperText="Helper text with details..."
+            label="Shipping Instructions Required Fields"
+            labelFor="text-input"
+            labelInfo="(required)"
+          >
+            <h3>Date</h3>
+            <FormContainer key={1} id="text-input1" placeholder="Day" />
+            <FormContainer placeholder="Month" />
+            <FormContainer placeholder="Year" />
+            <FormContainer placeholder="Date" />
+            <h3>Shipment</h3>
+            <FormContainer placeholder="Description" />
+            <FormContainer placeholder="Consigned To" />
+            <FormContainer placeholder="Bill of Lading Number" />
+            <FormContainer placeholder="Carrier Booking" />
+            <FormContainer placeholder="Consignor" />
+            <FormContainer placeholder="Email" />
+            <FormContainer placeholder="Telephone" />
+            <FormContainer placeholder="SCAC" />
+            <FormContainer placeholder="CID" />
+          </FormGroup>
+          <FormGroup label="Additional info">
+            <FormContainer placeholder="SID" />
 
-              <FormContainer placeholder="FOB" />
+            <FormContainer placeholder="FOB" />
 
-              <FormContainer placeholder="Amount" />
-              <FormContainer placeholder="Additional Info" />
-            </FormGroup>
-          </ChecklistColumn>
-        </Container> */}
-
-        {/* master form */}
-        <Container>
-          <ChecklistColumn>
-            <h1>Master Shipping Form</h1>
-            <FormGroup
-              helperText="Helper text with details..."
-              label="Shipping Instructions Required Fields"
-              labelFor="text-input"
-              labelInfo="(required)"
-            >
-              <FormContainer placeholder="Bill of Lading Number" />
-              <FormContainer placeholder="Freight Terms" />
-              <FormContainer placeholder="Export References" />
-              <h3>Shipper</h3>
-              <FormContainer placeholder="Shipper Name" />
-              <FormContainer placeholder="Shipper Address" />
-              <FormContainer placeholder="Shipper Contact Info" />
-              <h3>Consignee</h3>
-              <FormContainer placeholder="Consignee Name" />
-              <FormContainer placeholder="Consignee Address" />
-              <FormContainer placeholder="Consignee Contact Info" />
-              <h3>Notify Party</h3>
-              <FormContainer placeholder="Notify Party Name" />
-              <FormContainer placeholder="Notify Party Address" />
-              <FormContainer placeholder="Notify Party Contact Info" />
-              <h3>Other</h3>
-              <FormContainer placeholder="Shipper's Declaration of Value" />
-              <FormContainer placeholder="Express Release" />
-              <FormContainer placeholder="AES ITN" />
-              <FormContainer placeholder="HS Code" />
-              <FormContainer placeholder="Comments" />
-            </FormGroup>
-          </ChecklistColumn>
-        </Container>
-      </>
+            <FormContainer placeholder="Amount" />
+            <FormContainer placeholder="Additional Info" />
+          </FormGroup>
+        </ChecklistColumn>
+      </Container>
     );
   }
 }
