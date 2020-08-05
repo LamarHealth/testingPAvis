d4b1abcc8c3af9518904588ce6bb1a01ee7d6865
Carter Chen <carterchen@Carters-MBP.attlocal.net>
2020-08-05 18:28:25 -0400

    cleanup

--- client/tests/pageScale.test.js
+++ client/tests/pageScale.test.js
@@ -3,6 +3,7 @@ import mast3_33perScaled from "./page_scale/mast3_33perScaled.json";
 import mast3_50perScaled from "./page_scale/mast3_50perScaled.json";
 import mast3_75perScaled from "./page_scale/mast3_75perScaled.json";
 import mast3_100perNoScale from "./page_scale/mast3_100perNoScale.json";
+import mast3_150perScaled from "./page_scale/mast3_150perScaled.json";
 
 // these are to test PAGE_SCALE. the file 100per_noScale is what we get when we load a pdf with no zoom on the browser; these are the correct kvps. The other files are the output when we zoom out the browser page, at Xper zoom, using the scaled PAGE_SCALE from the formula in constants.js. If our scaling formula is working correctly, then the kvps should be the same as the correct kvps with no zoom, regardless of how far out of the browser you are zoomed.
 
@@ -11,6 +12,7 @@ const scaledKvps = {
   mast3_33perScaled,
   mast3_50perScaled,
   mast3_75perScaled,
+  mast3_150perScaled,
 };
 
 describe(`test PAGE_SCALE:`, () => {
