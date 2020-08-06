7cf11de69a1e9f696d3b267adac8c62ad837f209
Carter Chen <carterchen@Carters-MBP.attlocal.net>
2020-08-05 23:14:08 -0400

    cleanup

--- client/src/common/constants.ts
+++ client/src/common/constants.ts
@@ -1,7 +1,7 @@
 export const SIDEBAR_WIDTH = "25em";
 export const PAGE_SCALE = (1.1 * window.innerWidth) / 1440;
 export const DOC_IMAGE_WIDTH = window.innerWidth * (2 / 3);
-export const KONVA_MODAL_HEIGHT = window.innerHeight * (6 / 7);
+export const KONVA_MODAL_MAX_HEIGHT = window.innerHeight * (6 / 7);
 export const MODAL_WIDTH = 700;
 export const LOCAL_MODE = process.env.REACT_APP_LOCAL === "local";
 export const API_PATH =
