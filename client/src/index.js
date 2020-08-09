fd8d5c586b197c71011580c82a8bf5e3ce842928
Carter Chen <carterchen@Carters-MBP.attlocal.net>
2020-08-08 20:42:00 -0400

    bug fix

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,13 @@ import $ from "jquery";
 // Load dotenv
 import dotenv from "dotenv";
 import ShipmentsDashboard from "./shipments/ShipmentsDashboard";
-import { Sidebar } from "./shipments/Sidebar";
 import { RenderModal } from "./shipments/RenderModal";
 
 import { LOCAL_MODE } from "./common/constants";
 dotenv.config();
 
 $('<span id="insertion-point"/>').insertBefore(document.body);
-console.log(process.env);
+
 ReactDOM.render(
   <>
     <RenderModal />
