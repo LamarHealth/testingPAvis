fd42fa312d4a005d58f9e2f1cb8379d595b48751
reibs <areibman@gmail.com>
2023-04-26 21:16:45 -0700

    postfields debugging

--- client/src/pages/Background/index.ts
+++ client/src/pages/Background/index.ts
@@ -3,11 +3,25 @@
 import { OCRDocumentInfo, StatusCodes } from "../../types/documents";
 import { PDF_UPLOAD_BUCKET } from "../Content/common/constants";
 
+type PressignedURLResponse = {
+  presigned_post: {
+    url: string;
+    fields: {
+      key: string;
+      AWSAccessKeyId: string;
+      policy: string;
+      signature: string;
+      "x-amz-security-token": string;
+    };
+  };
+};
+
 type MessageRequest = {
   message?: string;
   data?: string;
   fillValue?: boolean;
   error?: boolean;
+  fileName?: string;
 };
 
 export interface OCRMessageResponse {
@@ -33,62 +47,121 @@ const base64ToBlob = (base64Data: string): Blob => {
 chrome.runtime.onMessage.addListener(
   (request: MessageRequest, sender, sendResponse) => {
     if (request.message === "fileUploaded") {
-      // 1. Receive the Base64 PDF data
+      // Receive the Base64 PDF data
       const base64Data = request.data || "";
       console.log("Received Base64 data:", base64Data);
-      // 2. Convert the Base64 data back to a Blob
-      const pdfFile = base64ToBlob(base64Data);
+      // Convert the Base64 data back to a Blob
+      const fileBlob = base64ToBlob(base64Data);
+
+      // Get filename from blob
+      const fileName = request.fileName || "";
+
+      // Convert blob to File
+      const pdfFile = new File([fileBlob], fileName);
 
       console.log(pdfFile);
 
-      // 3. Upload the PDF file
-      const formData = new FormData();
-      formData.append("pdf", pdfFile);
+      // First step:
+      // Get presigned POST URL from API
+      const presignedGeneratorURL =
+        "https://kuzoktnlpa.execute-api.us-east-1.amazonaws.com/default/doc-upload-url-generator";
 
       fetch(
-        `https://c4lcvj97v5.execute-api.us-east-1.amazonaws.com/default/plumbus-doc-upload`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ pdfData: base64Data }),
-        }
+        `${presignedGeneratorURL}?object_key=${encodeURIComponent(
+          pdfFile.name
+        )}`
       )
         .then((res) => res.json())
-        .then((res: OCRDocumentInfo) => {
-          console.log("Result");
+        .then((res: PressignedURLResponse) => {
+          console.log("res");
           console.log(res);
 
-          const docID = res.docID;
+          const postURL = res.presigned_post.url;
+          const postFields = res.presigned_post.fields;
+          // Create form data
+          // const formData = new FormData();
+          // formData.append("file", pdfFile);
+          console.log("---");
+          // @ts-ignore
+          // for (const [key, value] of formData.entries()) {
+          //   console.log(`${key}: ${value}`);
+          // }
+          console.log("---");
+          console.log(pdfFile);
+          console.log("formData.entries()");
+          console.log("postfields", postFields);
 
-          // If docID is not on the response, then there was an error
+          const poostFields = {
+            key: "GetFile (1).pdf",
+            AWSAccessKeyId: "ASIAUTAAG3ND3PRJQVHK",
+            "x-amz-security-token": "IQoJb3JpZ2luX2VjE...",
+            policy: "eyJleHBpc...",
+            signature: "FCqonUrxgiOcR6huzWUptP8I1l0=",
+          };
+          const formData = new FormData();
 
-          const response: OCRMessageResponse = !!docID
-            ? {
-                status: StatusCodes.SUCCESS,
-                message: "success",
-                documentInfo: res,
-              }
-            : {
-                status: StatusCodes.FAILURE,
-                message: res.message,
-                documentInfo: res,
-              };
+          // Add field data to formData
+          Object.entries(poostFields).forEach(([key, value]) => {
+            console.log("key", key);
+            console.log("value", value);
+            formData.append(key, value);
+          });
+          console.log("***");
+          // @ts-ignore
+          for (const [key, value] of formData.entries()) {
+            console.log(`${key}: ${value}`);
+          }
+          console.log("***");
 
-          sendResponse(response);
+          // Second step: Upload the file to the presigned POST URL
+          fetch(postURL, {
+            method: "POST",
+            body: formData,
+          })
+            .then((res) => res.json())
+            .then((res: OCRDocumentInfo) => {
+              console.log("Result");
+              console.log(res);
+
+              const docID = res.docID;
+
+              // If docID is not on the response, then there was an error
+
+              const response: OCRMessageResponse = !!docID
+                ? {
+                    status: StatusCodes.SUCCESS,
+                    message: "success",
+                    documentInfo: res,
+                  }
+                : {
+                    status: StatusCodes.FAILURE,
+                    message: res.message,
+                    documentInfo: res,
+                  };
+
+              sendResponse(response);
+            })
+            .catch((err) => {
+              console.error("Error uploading file to API:", err);
+              const response = { status: StatusCodes.FAILURE, message: err };
+              sendResponse(response);
+            })
+            .finally(() => {});
         })
         .catch((err) => {
-          console.error("Error uploading file to API:", err);
+          console.error("Error fetching presigned POST URL from API:", err);
           const response = { status: StatusCodes.FAILURE, message: err };
           sendResponse(response);
         })
         .finally(() => {});
+
       // Return true to indicate that you will send the response asynchronously
       return true;
     }
   }
 );
 
-// Get PDF from API
+// TODO: Get PDF from API
 chrome.runtime.onMessage.addListener(
   (request: MessageRequest, sender, sendResponse) => {
     if (request.message === "fileRequest") {
