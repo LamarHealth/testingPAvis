1467220f77cb68cc0ee6764daee0c60e2e96608c
sharmaee <56173708+sharmaee@users.noreply.github.com>
2023-10-18 10:08:48 -0400

    Update functions.ts

--- client/src/pages/Content/components/ScoreChiclet/functions.ts
+++ client/src/pages/Content/components/ScoreChiclet/functions.ts
@@ -186,6 +186,37 @@ export const replaceAndSetNewMounter = (
  * elements with existing text or overwrite all
  * @param {KeyValuesByDoc} [keyValuePairs] - KVP values extracted from OCR
  */
+
+const radioButtonMapping: { [key: string]: string } = {
+  'S1-patient_gender': 'gender',
+  // ... add other manual mappings as needed for first form
+};
+
+// Array of variations for the word "gender" or "sex"
+const genderVariations = ['gender', 'gender:', 'Gender', 'Gender:', 'sex', 'sex:', 'Sex', 'Sex:'];
+
+// Mapping for questions and their potential answers
+const questionAnswerMapping: { [question: string]: string[] } = {
+  'Gender': ['male', 'female', 'other'],
+  'Is this is a continuation of therapy?': ["yes", "no"],
+  'Is this medication being used to treat a chronic or long term condition for which this prescription medication may be necessary for the life of the patient?' : ['yes', 'no']
+};
+
+function shouldCheckRadioButtonForQA(question: string, answer: string, keyValuePairs: KeyValuePairs): boolean {
+
+  const potentialAnswers = questionAnswerMapping[question];
+
+  if (!potentialAnswers) return false;
+
+  const matchingKeyValuePair = Object.entries(keyValuePairs).find(([key, value]) => {
+    return potentialAnswers.includes(value.trim().toLowerCase()) && key.trim().toLowerCase() === question.trim().toLowerCase();
+  });
+
+  console.log("Match found:", !!matchingKeyValuePair);
+
+  return !!matchingKeyValuePair && matchingKeyValuePair[1].toLowerCase() === answer.toLowerCase();
+}
+
 export const populateForms = (
   action: PopulateFormsActionTypes,
   keyValuePairs: KeyValuePairs
@@ -199,13 +230,21 @@ export const populateForms = (
       handleFreightTerms(el, keyValuePairs);
     });
     $(DOCIT_TAG).each((ndx, el) => {
+      
+      // Check if the element's name attribute is 'patient_address2' or 'provider_address2'
+      if ($(el).attr('name') === 'patient_address2' || $(el).attr('name') === 'provider_address2') {
+        return; // Skip the current iteration and move to the next element
+      }
+
       const targetString = assignTargetString(el);
       const sortedKeyValuePairs = getEditDistanceAndSort(
         keyValuePairs,
         targetString,
         "lc substring"
       );
-      const elText = $(el).prop("value");
+      //const elText = $(el).prop("value");
+      const elText = $(el).val();
+
       if (!!!elText && action === PopulateFormsActionTypes.overwriteBlank) {
         if (hasGoodHighestMatch(sortedKeyValuePairs)) {
           renderChiclets(
@@ -213,13 +252,102 @@ export const populateForms = (
             el,
             sortedKeyValuePairs[0]
           );
-          $(el).prop("value", sortedKeyValuePairs[0]["value"]);
+          // $(el).prop("value", sortedKeyValuePairs[0]["value"]);
+          //$(el).val(sortedKeyValuePairs[0]["value"]).trigger("change");
+          $(el).trigger('click')
+            .trigger('focus')
+            .val(sortedKeyValuePairs[0]["value"]);
+
+          let charToSimulate = sortedKeyValuePairs[0]["value"].charAt(0);
+
+          // Simulate the keystrokes
+          let keydownEvent = new KeyboardEvent("keydown", {
+            bubbles: true,
+            key: charToSimulate,
+            keyCode: charToSimulate.charCodeAt(0),
+            which: charToSimulate.charCodeAt(0)
+          });
+          el.dispatchEvent(keydownEvent);
+
+          let keypressEvent = new KeyboardEvent("keypress", {
+            bubbles: true,
+            key: charToSimulate,
+            keyCode: charToSimulate.charCodeAt(0),
+            which: charToSimulate.charCodeAt(0)
+          });
+          el.dispatchEvent(keypressEvent);
+
+          let keyupEvent = new KeyboardEvent("keyup", {
+            bubbles: true,
+            key: charToSimulate,
+            keyCode: charToSimulate.charCodeAt(0),
+            which: charToSimulate.charCodeAt(0)
+          });
+          el.dispatchEvent(keyupEvent);
+
+          $(el).trigger('blur');
+          $('body').trigger('click');
+
         } else {
           renderChiclets(RenderChicletsActionTypes.blank, el);
-          $(el).prop("value", null);
+          //$(el).prop("value", null);
+          $(el).val("").trigger("change");
         }
       }
     });
+    
+    $("input[type='radio']").each((ndx, el) => {
+      const radioButtonName = $(el).attr("name");
+      const radioButtonValue = $(el).val();
+
+      const parent = el.closest('.nw_row') || el.parentElement;
+
+      if (parent) {
+        const questionElement = parent.querySelector('.question, .nw_title');
+
+        // Check if questionElement is not null before accessing its properties
+        const questionText = questionElement && questionElement.textContent ? questionElement.textContent.trim() : null;
+
+        const labelElement = el.closest('label');
+
+        // Check if labelElement is not null before accessing its properties
+        const labelText = labelElement?.textContent?.replace(el.outerHTML, '').trim() ?? null;
+
+        if (questionText && labelText && shouldCheckRadioButtonForQA(questionText, labelText, keyValuePairs) && action === PopulateFormsActionTypes.overwriteBlank) {
+          // Simulate user interactions to select the radio button
+          $(el).trigger('focus').trigger('mousedown').trigger('mouseup').trigger('click');
+          $(el).prop("checked", true);
+          $(el).trigger('change').trigger('blur');
+        }
+      }
+  
+      if (typeof radioButtonName === "string" && typeof radioButtonValue === "string") {
+        if (shouldCheckRadioButtonForQA(radioButtonName, radioButtonValue, keyValuePairs) && action === PopulateFormsActionTypes.overwriteBlank) {
+            
+            // Simulate a focus on the radio button
+            $(el).trigger('focus');
+            
+            setTimeout(() => {
+                // Simulate mousedown, mouseup, and click events in sequence
+                $(el).trigger('mousedown');
+                $(el).trigger('mouseup');
+                $(el).trigger('click');
+    
+                // Check the radio button
+                $(el).prop("checked", true);
+    
+                // Trigger the change event
+                $(el).trigger('change');
+    
+                // Simulate a blur to mimic user interactions
+                $(el).trigger('blur');
+    
+            }, 100); // Introducing a 100ms delay
+        }
+    }
+    
+  });
+  
     window.addEventListener("resize", positionAllMounters); // no need remove listener, as identical listeners are automatically discarded
   });
 };
